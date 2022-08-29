b8f084d65a6f13a335532a9bf6dd86548f91e784
edsonmier <edson_mier28@hotmail.com>
2022-08-29 04:35:25 -0500

    update: erenFragment

--- Activity4/app/src/main/java/com/example/activity4/MainActivity.java
+++ Activity4/app/src/main/java/com/example/activity4/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.activity4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentTransaction transaction;
+    Fragment erenInfo, mikasaInfo, leviInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        erenInfo = new ErenInfo();
+    }
+
+    public void onClick(View view) {
+        transaction=getSupportFragmentManager().beginTransaction();
+        switch (view.getId()){
+            case R.id.button: transaction.replace(R.id.fragment_container, erenInfo);
+            transaction.addToBackStack(null); break;
+        }
+        transaction.commit();
     }
 }
